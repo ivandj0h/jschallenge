24584a57315d1cde822f1419bfb1e67801b28045
ivandi1980 <ivandi.djoh@gmail.com>
2020-07-03 20:45:36 +0700

    reactjs_snippet

---
+++ react_list_object.js
@@ -0,0 +1,42 @@
+// ReactJS
+
+// App.js
+/*
+import React from 'react';
+
+const users = [
+    {
+        id: 1,
+        name: 'Robin Williams',
+        occupation: 'teacher',
+    },
+    {
+        id: 2,
+        name: 'John Doe',
+        occupation: 'gardener',
+    },
+];
+
+const WordList = () => (
+    <ul>
+        {users.map(user => (
+            <li key={user.id}>
+                <div>{user.id}</div>
+                <div>{user.name}</div>
+                <div>{user.occupation}</div>
+            </li>
+        ))}
+    </ul>
+);
+
+export default WordList;
+*/
+
+// index.js
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import WordList from './App';
+
+ReactDOM.render(<WordList />, document.getElementById('root'));
+*/
